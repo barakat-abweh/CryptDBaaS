700ffc609f47c8031124265da8e70977c35a897c
Barakat Abwe <brkatabweabwe@gmail.com>
2018-07-10 03:58:03 +0000

    Update NLP/SKW.java, NLP/RakeLanguages.java, NLP/PorterAlgo.java, NLP/en.txt, en.txt, Encryption/ValueRange.java, Encryption/RSA_Enc_Dec.java, Encryption/Ope.java, Encryption/Hgd.java, Encryption/en.txt, Encryption/Coins.java, Encryption/Byte_Hex_Transform.java, Encryption/BigDecimalUtils.java, Encryption/AES_Key_Generator.java, Encryption/AES_Enc_Dec.java files

---
+++ Encryption/ValueRange.java
@@ -0,0 +1,63 @@
+package Encryption;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author Savvas Savvides <savvas@purdue.edu>
+ *
+ */
+public class ValueRange {
+
+	BigInteger start;
+	BigInteger end;
+
+	public ValueRange(BigInteger s, BigInteger e) {
+		this.start = s;
+		this.end = e;
+
+		if (this.start.compareTo(this.end) > 0)
+			throw new RuntimeException("start > end");
+
+	}
+
+	/**
+	 * Copy constructor
+	 *
+	 * @param other
+	 */
+	public ValueRange(ValueRange other) {
+		this(other.start, other.end);
+	}
+
+	/**
+	 * the range length, including its start and end
+	 *
+	 * @return
+	 */
+	public BigInteger size() {
+		return this.end.subtract(this.start).add(BigInteger.ONE);
+	}
+
+	/**
+	 * Return a number of bits required to encode any value within the range
+	 *
+	 * @return
+	 */
+	public long rangeBitSize() {
+		return (long) Math.ceil(Math.log(this.size().longValueExact()) / Math.log(2));
+	}
+
+	public boolean contains(BigInteger number) {
+		return number.compareTo(this.start) >= 0 && number.compareTo(this.end) <= 0;
+	}
+
+}
